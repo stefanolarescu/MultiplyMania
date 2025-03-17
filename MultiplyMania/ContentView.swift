0b8a6fb55d46e580d028fe608533f885e69c044a
Stefan Olarescu <stefanolarescu@gmail.com>
2025-03-17 22:39:06 +0200

    Created Xcode project.

---
+++ MultiplyMania/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MultiplyMania
+//
+//  Created by Stefan Olarescu on 17.03.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MultiplyMania/MultiplyManiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplyManiaApp.swift
+//  MultiplyMania
+//
+//  Created by Stefan Olarescu on 17.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct MultiplyManiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
