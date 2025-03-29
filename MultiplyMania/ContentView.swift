daadbf19acb5aa290a4b30651f289eb61c6be115
Stefan Olarescu <stefanolarescu@gmail.com>
2025-03-29 13:59:57 +0200

    Implemented app.

--- MultiplyMania/ContentView.swift
+++ MultiplyMania/ContentView.swift
@@ -8,14 +8,79 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var multiplicationTable = 1
+    @State private var numberOfQuestions = 5
+    
+    @State private var hasStartedGame = false
+    @State private var question = ""
+    @State private var numberOfQuestionsAsked = Int.zero
+    
+    @State private var answer = ""
+    
+    private let questionsCounts = [5, 10, 20]
+    
+    private let questionFormat = "What is %@ times %@?"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            Form {
+                if hasStartedGame {
+                    Section("Question #\(numberOfQuestionsAsked)") {
+                        Text(question)
+                        
+                        TextField("Answer:", text: $answer)
+                            .keyboardType(.numberPad)
+                            .onChange(of: answer) { newValue in
+                                answer = newValue.filter { $0.isNumber }
+                            }
+                    }
+                    
+                    Section {
+                        Button(numberOfQuestionsAsked == numberOfQuestions ? "Finish" : "Next") {
+                            askQuestion()
+                        }
+                        .disabled(answer.isEmpty)
+                    }
+                } else {
+                    Section("Settings") {
+                        Stepper(
+                            "Multiplication table: \(multiplicationTable)",
+                            value: $multiplicationTable,
+                            in: 1...10
+                        )
+                        
+                        Picker(
+                            "Number of questions",
+                            selection: $numberOfQuestions,
+                            content: {
+                                ForEach(questionsCounts, id: \.self) {
+                                    Text("\($0)")
+                                }
+                            }
+                        )
+                    }
+                    
+                    Section {
+                        Button("Start") {
+                            hasStartedGame = true
+                            
+                            askQuestion()
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Multiply Mania")
         }
-        .padding()
+    }
+    
+    private func askQuestion() {
+        if numberOfQuestionsAsked == numberOfQuestions {
+            hasStartedGame = false
+            numberOfQuestionsAsked = .zero
+        } else {
+            question = String(format: questionFormat, "\(multiplicationTable)", "\(Int.random(in: 1...10))")
+            numberOfQuestionsAsked += 1
+        }
     }
 }
 
